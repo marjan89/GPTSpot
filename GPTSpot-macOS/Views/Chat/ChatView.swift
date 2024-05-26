e5ceefc70fb4f8d1ee36a4b38fbb05db44591fd3
sinisa <marjan89@gmail.com>
2024-05-27 01:26:05 +0200

    chat view chatControls function refactor

--- GPTSpot-macOS/Views/Chat/ChatView.swift
+++ GPTSpot-macOS/Views/Chat/ChatView.swift
@@ -77,83 +77,128 @@ struct ChatView: View {
     }
 
     @ViewBuilder
+    func saveAsTemplateButton() -> some View {
+        if !chatViewService.prompt.isEmpty || !templateSearchQuery.isEmpty {
+            Button("", systemImage: "square.and.arrow.down.fill") {
+                if showTemplateStripe {
+                    modelContext.insert(Template(content: templateSearchQuery))
+                    templateSearchQuery = ""
+                } else {
+                    chatViewService.savePrompAsTemplate()
+                }
+            }
+            .accessibilityLabel("Save as template")
+            .help("Save as template")
+            .keyboardShortcut(.init("s", modifiers: [.command]))
+            .buttonStyle(BorderlessButtonStyle())
+        }
+    }
+
+    @ViewBuilder
+    func promptPrefixToggle() -> some View {
+        Toggle(
+            isOn: $promptPrefix,
+            label: {
+                Text("Prompt prefix")
+            }
+        )
+        .toggleStyle(SwitchToggleStyle())
+        .keyboardShortcut(.init("/"))
+    }
+
+    @ViewBuilder
+    func stopGeneratingContentButton() -> some View {
+        if chatViewService.generatingContent {
+            Button("", systemImage: "stop.fill") {
+                chatViewService.cancelCompletion()
+            }
+            .accessibilityLabel("Cancel response")
+            .help("Cancel response")
+            .keyboardShortcut(.init(.return, modifiers: [.command, .shift]))
+            .buttonStyle(BorderlessButtonStyle())
+        }
+    }
+
+    @ViewBuilder
+    func helpButton() -> some View {
+        Button("", systemImage: "questionmark.circle.fill") {
+            showHelpRibbon.toggle()
+        }
+        .accessibilityLabel("Show help")
+        .help("Show help")
+        .keyboardShortcut(.init("?"))
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    @ViewBuilder
+    func statsButton() -> some View {
+        Button("", systemImage: "chart.bar.fill") {
+            showStats.toggle()
+        }
+        .accessibilityLabel("Show stats")
+        .help("Show stats")
+        .keyboardShortcut(.init("."))
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    @ViewBuilder
+    func trashButton() -> some View {
+        Button("", systemImage: "trash.fill") {
+            chatViewService.discardHistory(for: workspace)
+        }
+        .accessibilityLabel("Discard history")
+        .help("Discard history")
+        .keyboardShortcut(.init("d"))
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    @ViewBuilder
+    func templatesButton() -> some View {
+        Button("", systemImage: "folder.fill") {
+            showTemplateStripe.toggle()
+        }
+        .accessibilityLabel("Show templates")
+        .help("Show templates")
+        .keyboardShortcut(.init("t"))
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    @ViewBuilder
+    func lastMessageAsPrompt() -> some View {
+        Button("", systemImage: "memories") {
+            focusedField = true
+            chatViewService.setLastChatMessageAsPrompt(workspace: workspace)
+        }
+        .accessibilityLabel("Set last message as prompt")
+        .help("Set last message as prompt")
+        .keyboardShortcut(.upArrow)
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    @ViewBuilder
+    func sendButton() -> some View {
+        Button("", systemImage: "paperplane.fill") {
+            chatViewService.executePrompt(workspace: workspace)
+        }
+        .accessibilityLabel("Send")
+        .help("Send")
+        .keyboardShortcut(.return)
+        .buttonStyle(BorderlessButtonStyle())
+    }
+
+    @ViewBuilder
     func chatControls() -> some View {
         HStack {
             Spacer()
-            if !chatViewService.prompt.isEmpty || !templateSearchQuery.isEmpty {
-                Button("", systemImage: "square.and.arrow.down.fill") {
-                    if showTemplateStripe {
-                        modelContext.insert(Template(content: templateSearchQuery))
-                        templateSearchQuery = ""
-                    } else {
-                        chatViewService.savePrompAsTemplate()
-                    }
-                }
-                .accessibilityLabel("Save as template")
-                .help("Save as template")
-                .keyboardShortcut(.init("s", modifiers: [.command]))
-                .buttonStyle(BorderlessButtonStyle())
-            }
-            Toggle(
-                isOn: $promptPrefix,
-                label: {
-                    Text("Prompt prefix")
-                }
-            )
-            .toggleStyle(SwitchToggleStyle())
-            .keyboardShortcut(.init("/"))
-            if chatViewService.generatingContent {
-                Button("", systemImage: "stop.fill") {
-                    chatViewService.cancelCompletion()
-                }
-                .accessibilityLabel("Cancel response")
-                .help("Cancel response")
-                .keyboardShortcut(.init(.return, modifiers: [.command, .shift]))
-                .buttonStyle(BorderlessButtonStyle())
-            }
-            Button("", systemImage: "questionmark.circle.fill") {
-                showHelpRibbon.toggle()
-            }
-            .accessibilityLabel("Show help")
-            .help("Show help")
-            .keyboardShortcut(.init("?"))
-            .buttonStyle(BorderlessButtonStyle())
-            Button("", systemImage: "chart.bar.fill") {
-                showStats.toggle()
-            }
-            .accessibilityLabel("Show stats")
-            .help("Show stats")
-            .keyboardShortcut(.init("."))
-            .buttonStyle(BorderlessButtonStyle())
-            Button("", systemImage: "trash.fill") {
-                chatViewService.discardHistory(for: workspace)
-            }
-            .accessibilityLabel("Discard history")
-            .help("Discard history")
-            .keyboardShortcut(.init("d"))
-            .buttonStyle(BorderlessButtonStyle())
-            Button("", systemImage: "folder.fill") {
-                showTemplateStripe.toggle()
-            }
-            .accessibilityLabel("Show templates")
-            .help("Show templates")
-            .keyboardShortcut(.init("t"))
-            .buttonStyle(BorderlessButtonStyle())
-            Button("", systemImage: "memories") {
-                focusedField = true
-                chatViewService.setLastChatMessageAsPrompt(workspace: workspace)
-            }
-            .accessibilityLabel("Set last message as prompt")
-            .help("Set last message as prompt")
-            .keyboardShortcut(.upArrow)
-            .buttonStyle(BorderlessButtonStyle())
-            Button("", systemImage: "paperplane.fill") {
-                chatViewService.executePrompt(workspace: workspace)
-            }
-            .accessibilityLabel("Send")
-            .help("Send")
-            .keyboardShortcut(.return)
-            .buttonStyle(BorderlessButtonStyle())
+            saveAsTemplateButton()
+            promptPrefixToggle()
+            stopGeneratingContentButton()
+            helpButton()
+            statsButton()
+            trashButton()
+            templatesButton()
+            lastMessageAsPrompt()
+            sendButton()
         }
     }
 }
